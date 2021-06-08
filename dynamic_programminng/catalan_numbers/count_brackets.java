c6912ba35c545b82925527fcce0907bc0289776c
Utkarsh Bahukhandi <utkarshbahukhandi0002@gmail.com>
2021-06-08 12:08:29 +0530

    commit

---
+++ dynamic_programminng/catalan_numbers/count_brackets.java
@@ -0,0 +1,36 @@
+package catalan_numbers;
+import java.io.*;
+import java.util.*;
+public class count_brackets {
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        
+        int n=sc.nextInt();
+        
+        
+        if(n==0||n==1){
+            System.out.println(1);
+            return;
+        }
+        
+        int[] dp=new int[n+1];
+        
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            int inside=i-1;
+            int outside=0;
+            
+            while(inside>=0){
+                dp[i]+=dp[inside]*dp[outside];
+                inside--;
+                outside++;
+            }
+        }
+        
+        
+        System.out.println(dp[n]);
+   
+    }        
+}
---
+++ dynamic_programminng/include_exclude_style/alternating_sequence.java
@@ -0,0 +1,110 @@
+package include_exclude_style;
+
+java.util.*;
+public class alternating_sequence {
+    
+    public class Pair{
+        int val;
+        int sign;
+        
+        public Pair(){
+            val=1;
+            sign=1;
+        }
+        
+    }
+    public int wiggleMaxLength(int[] nums) {
+        /*int n=nums.length;
+        
+        if(n==1) {
+            return n;
+        }
+        if(n==2){
+            if(nums[0]!=0 && nums[1]!=0){
+                return 2;
+            }
+            return 1;
+        }
+        
+        Pair[] dp=new Pair[n];
+        for(int i=0;i<n;i++){
+            dp[i]=new Pair();
+        }
+        
+        
+        
+       
+        dp[0].val=1;
+        dp[0].sign=(int)Math.signum(nums[0]);
+        
+        for(int i=1;i<n;i++){
+            
+        
+            for(int j=0;j<=i-1;j++){
+            
+                if(dp[j].val==1){
+                    if(dp[j].sign == 0 && (int)Math.signum(nums[i]-nums[j])==0){
+                        dp[i].val=1;
+                        dp[i].sign=0;
+                    }       
+                    else{
+                        if(dp[i].val< 2 && (int)Math.signum(nums[i]-nums[j])!=0){
+                            dp[i].val=2;
+                            dp[i].sign=(int)Math.signum(nums[i]-nums[j]);
+                        }
+                        else if(dp[i].val< 2 && (int)Math.signum(nums[i]-nums[j])==0){
+                            dp[i].val=1;
+                            dp[i].sign=(int)Math.signum(nums[i]-nums[j]);
+                        }
+                    }
+                }
+                else{
+                     if((int)Math.signum(nums[i]-nums[j])!=0  && ((int)Math.signum(nums[i]-nums[j])!= dp[j].sign)){
+                if(dp[i].val<1+dp[j].val){
+                    dp[i].val=1+dp[j].val;
+                    dp[i].sign=(int)Math.signum(nums[i]-nums[j]);
+                }
+            }
+                }
+                    
+            
+            }
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(max<dp[i].val){
+                max=dp[i].val;
+            }
+        }
+        
+        return max;*/
+         int val=1;
+        
+        int[] up=new int[nums.length];
+        int[] down=new int[nums.length];
+        up[0]=1;
+        down[0]=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[i-1]){
+                up[i]=down[i-1]+1;
+                down[i]=down[i-1];
+            }
+            else if(nums[i]<nums[i-1]){
+                up[i]=up[i-1];
+                down[i]=up[i-1]+1;
+            }
+            else{
+                up[i]=up[i-1];
+                down[i]=down[i-1];
+            }
+        }
+        int max=Integer.MIN_VALUE;
+    for(int i=0;i<nums.length;i++){
+        int m=Math.max(up[i],down[i]);
+        
+        max=Math.max(m,max);
+    }    
+        return max;
+        
+    }
+}
